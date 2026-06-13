ae7a2674d9b8929d80bae02567aa2659a5f615da
Amit Bansode <amitb@creativegalileo.com>
2026-06-13 17:49:55 +0530

    flow operators

--- KotlinFlowPart_1/app/src/main/java/com/kotlin/kotlin_flow_part_1/MainViewModel.kt
+++ KotlinFlowPart_1/app/src/main/java/com/kotlin/kotlin_flow_part_1/MainViewModel.kt
@@ -5,7 +5,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onEmpty
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -15,7 +22,7 @@ class MainViewModel : ViewModel() {
         emit(startingValue)
         var currentValue = startingValue
         while (currentValue > 0) {
-            delay(1000L.milliseconds)
+            delay(1000.milliseconds)
             currentValue--
             emit(currentValue)
         }
@@ -27,10 +34,35 @@ class MainViewModel : ViewModel() {
 
     fun getCountdownTime() {
         viewModelScope.launch {
-            countDownTimer.collectLatest { time ->
-                delay(1500L.milliseconds)
-                Log.d("count", "getCountdownTime: $time")
-            }
+
+            val count = countDownTimer
+                .filter {
+                    it % 2 == 0
+                }
+                .map {
+                    it * it
+                }
+                .onStart {
+                    Log.d("count", "getCountdownTime: Started")
+                    emit(-1)
+                }
+                .onEach {
+                    Log.d("count", "getCountdownTime: onEach $it")
+                }
+                .onEmpty {
+                    Log.d("count", "getCountdownTime: Empty")
+                }
+                .onCompletion {
+                    Log.d("count", "getCountdownTime: Completed")
+                }
+                .count {
+                    it % 2 == 0
+                }
+                // operator after count will not work
+
+            Log.d("count", "getCountdownTime: $count")
         }
+
+
     }
 }
