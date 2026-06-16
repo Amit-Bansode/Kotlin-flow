d80b712baff6df0906c5b6d2c7582a3ca5a76ed7
Amit Bansode <amitb@creativegalileo.com>
2026-06-16 18:06:49 +0530

    conflate

--- KotlinFlowPart_1/app/src/main/java/com/kotlin/kotlin_flow_part_1/MainViewModel.kt
+++ KotlinFlowPart_1/app/src/main/java/com/kotlin/kotlin_flow_part_1/MainViewModel.kt
@@ -4,9 +4,16 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.fold
@@ -33,11 +40,62 @@ class MainViewModel : ViewModel() {
     }
 
     val elements = flowOf(10, 20, 30)
-
+    val flow1 = (1..5).asFlow()
+    val flow2 = (3..4).asFlow()
 
     init {
 //        getCountdownTime()
         getElements()
+        //flow transform
+        getCombinedFlow()
+        //flow filter
+        collectFlow()
+    }
+
+    fun collectFlow() {
+        val flow = flow {
+            delay(1000L)
+            emit("Appetizer")
+            delay(1500L)
+            emit("Main dish")
+            delay(500L)
+            emit("Dessert")
+        }
+        //Buffer -> buffer the emitted items, so that processing is not blocked. operators like buffer to preserve the emitted items. and to process sequentially
+        //Conflate -> only the last value is processed
+        //CollectLatest -> only the last value is processed
+
+        viewModelScope.launch {
+            flow.onEach {
+                Log.d("TAG", "collectFlow: $it is delivered")
+            }.conflate()
+                .collect {
+                    Log.d("TAG", "collectFlow: Now eating $it")
+                    delay(5000L)
+                    Log.d("TAG", "collectFlow: Finished eating $it")
+                }
+        }
+
+
+    }
+
+
+    fun getCombinedFlow() {
+        viewModelScope.launch {
+            //flatMapMerge
+            //flatMapConcat
+            flow1.flatMapLatest { f1 ->
+                flow {
+                    emit(f1)
+                    Log.d("TAG", "flatMapConcat: Start $f1")
+                    delay(1000.milliseconds)
+                    emit(f1)
+                    Log.d("TAG", "flatMapConcat:End $f1")
+                }
+            }.collect {
+                Log.d("TAG", "getCombinedFlow: $it")
+            }
+        }
     }
 
     fun getCountdownTime() {
